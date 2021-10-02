833169389c48871dbe09a69bc0e8d2e73a300c84
Sparsh Kumar <sparshkumar14998@gmail.com>
2021-10-02 15:14:15 +0530

    sequelize integration and database configuration done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    DatabaseModule,
+    ConfigModule.forRoot ({ isGlobal: true })
+  ],
 })
 
 export class AppModule {}
